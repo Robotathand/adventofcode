82a5807f522e9deb3454f2ffa8b02c8aecea62bd
Robotathand@slug <robotathand@gmail.com>
2022-12-13 22:27:11 +0000

    add 2022/2/2

--- 2022/2/2.py
+++ 2022/2/2.py
@@ -3,24 +3,24 @@ count = 0
 
 f = open("data.txt", "r")
 for x in f:
-   if x == "A X\n": #draw(3) + rock(1) 
-      points = points + 4
-   elif x == "A Y\n":  #win(6) + paper(2)
-      points = points + 8
-   elif x == "A Z\n": #loss(0) + scissors(3) 
+   if x == "A X\n": 
       points = points + 3
-   elif x == "B X\n": #loss(0) + rock(1) 
+   elif x == "A Y\n":
+      points = points + 4
+   elif x == "A Z\n":
+      points = points + 8
+   elif x == "B X\n":
       points = points + 1
-   elif x == "B Y\n": #draw(3) + paper(2) 
+   elif x == "B Y\n":
       points = points + 5
-   elif x == "B Z\n": #win(6) + scissors(3) 
+   elif x == "B Z\n":
       points = points + 9
-   elif x == "C X\n": #win(6) + rock(1) 
-      points = points + 7
-   elif x == "C Y\n": #loss(0) + paper(2)
+   elif x == "C X\n": 
       points = points + 2
-   elif x == "C Z\n": #draw(3) + scissors(3)
+   elif x == "C Y\n":
       points = points + 6
+   elif x == "C Z\n":
+      points = points + 7
    else:
        print("ERROR")
 #   print(x, points)
