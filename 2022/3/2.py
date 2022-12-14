fd3891b62a18756a5bdf8675fb029cdfbc965901
Robotathand@slug <robotathand@gmail.com>
2022-12-14 08:29:57 +0000

    almost done 2022/03/2

---
+++ 2022/3/2.py
@@ -0,0 +1,25 @@
+three = 1
+total = 0
+
+f = open("data.txt", "r")
+for x in f:
+    if three == 1:
+        r1 = x.strip()
+    elif three == 2:
+        r2 = x.strip()
+    elif three == 3:
+        r3 = x.strip()
+        strings = [r1, r2, r3]
+        cc = set.intersection(*map(set,strings))
+        print(cc, str(cc).strip())
+        if str(cc).isupper() == True:
+            priority = ord(str(cc)[2]) - 38
+        else:
+            priority = ord(str(cc)[2]) - 96
+        total = total + priority
+        three = 1
+    else:
+        print("error: ", x)
+    three = three + 1
+
+print(total)
