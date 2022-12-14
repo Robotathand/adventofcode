fc08f4533a68a1324a7ef3af9b5ed857c60428a2
Robotathand <97321601+Robotathand@users.noreply.github.com>
2022-12-14 21:47:05 +0000

    almost done 2022/04/1

--- 2022/4/1.py
+++ 2022/4/1.py
@@ -1,4 +1,17 @@
+count = 0
+
 f = open("data.txt", "r")
 for x in f:
-    pair = x.split(",")
-    print(pair)
+    pair = x.strip().split(",")
+#    print(pair)
+    a = pair[0].split("-")
+    b = pair[1].split("-")
+    aa = a[0]
+    ab = a[1]
+    ba = b[0]
+    bb = b[1]
+    if aa <= ba and ab >= bb or ba <= aa and bb >= ab:
+        count = count + 1
+#    print(pair, a, b, aa, ab, ba, bb, count)
+
+print(count)
