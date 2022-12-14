9341dd3933b9b3e0673892418bd605cb3914255a
Robotathand@slug <robotathand@gmail.com>
2022-12-14 00:28:21 +0000

    carry on code 2022/3/1

--- 2022/3/1.py
+++ 2022/3/1.py
@@ -6,5 +6,8 @@ for x in f:
     lenth = len(rucksack)
     c1 = rucksack[0:int(lenth / 2)]
     c2 = rucksack[int(lenth / 2 ):lenth]
+    cc = ''.join(
+        set(c1).intersection(c2)
+    )
 
-print(rucksack, c1, c2)
+print(rucksack, c1, c2, cc)
