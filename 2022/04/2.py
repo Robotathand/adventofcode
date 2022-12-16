c59785774a1106eaf592527552376ca59e14fe60
Robotathand@slug <robotathand@gmail.com>
2022-12-16 23:18:45 +0000

    finish 2022/04/*

--- 2022/04/2.py
+++ 2022/04/2.py
@@ -7,8 +7,10 @@ for line in open("data.txt"):
     a, b = map(int, pair[0].split("-"))
     x, y = map(int, pair[1].split("-"))
 
-    if a <= x and b >= y or x <= a and y >= b:
+    if a >= x and a <= y or b >= x and b <= y or x >= a and x <= b or y >= a and y <= b:
         count += 1
+
+#    print(line, a, b, x, y, count)
 
 
 print(count)
