9e6f9a1f20bc0ea319d858f8819112874fbe0b08
Robotathand <robotathand@gmail.com>
2023-12-01 21:36:08 +0000

    start 2013-1-1

--- 2023/01/1.py
+++ 2023/01/1.py
@@ -1,13 +1,29 @@
 t = 0
+s = []
 a = 0
 b = 0
+br = False
 
 f = open("data.txt", "r")
 
 for l in f:
     for c in l:
-        if c.isnumeric() == True:
-            a = c
-            for c in l:
-                if c.isnumeric():
-                    
+        s.append(c)
+
+        
+    for i in range(len(s)):
+        if s[int(i)].isnumeric():
+            a = s[i]
+            br = True
+            break
+
+    for i in range(len(s)):
+        if s[(len(s) - 1) - i].isnumeric:
+            b = s[(len(s) - 1) - i]
+            br = True
+            s = []
+            break
+    
+    t = int(str(a) + str(b))
+
+print(t)
