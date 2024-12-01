6cea31fd13cc7faedbfb8a84170c1bfd633bcbf9
robotathand <robotathand@gmail.com>
2024-12-01 22:25:57 +0000

    done 2024/01/1

--- 2024/01/part1/src/main.rs
+++ 2024/01/part1/src/main.rs
@@ -1,62 +1,33 @@
 use std::fs;
 
 fn main() {
-    let mut c1: Vec<i32> = Vec::new();
-    let mut c2: Vec<i32> = Vec::new();
-    // let mut t1 = 0;
-    // let mut t2 = 0;
-    let mut d = 0;
+    let mut column1: Vec<i32> = Vec::new();
+    let mut column2: Vec<i32> = Vec::new();
+    let mut distance = 0;    
 
     let data = fs::read_to_string("../data.txt")
         .expect("Can't read");
 
-    // println!("{data}");
-
     for l in data.lines() {
-        let v1 = l[..1].parse().unwrap();
-        let v2 = l[4..].parse().unwrap();
-        c1.push(v1);
-        c2.push(v2);
-        println!("{v1}, {v2}");
+        let mut line = l.split_whitespace();
+        column1.push(
+            line.next().unwrap().parse::<i32>().unwrap(),
+        );
+        column2.push(
+            line.next().unwrap().parse::<i32>().unwrap(),
+        );
     }
 
-    /* for i in c1 {
-        t1 += i;
-    }
-    for i in c2 {
-        t2 += i;
-    }
-
-    println!("Total: {t1}, {t2}");
-
-    let d = t2 - t1; */
-
-    c1.sort();
-    c2.sort();
-
-    for i in &c1 {
-        println!("C1: {i}");
-    }
-
-    for i in &c2 {
-        println!("C2: {i}");
-    }
-
-    for i in 0..c1.len() {
-        let q = c1[i];
-        let w = c2[i];
-        println!("L: {}, {}", q, w);
-        if c1[i] > c2[i] {
-            d += c1[i] - c2[1];
-            let a = c1[i] - c2[1];
-            println!("A: {a}");
-        } else if c1[i] < c2[i] {
-            d += c2[i] - c1 [i];
-            let a = c2[i] - c1[1];
-            println!("B: {a}");
+    column1.sort();
+    column2.sort();
+    
+    for i in 0..column1.len() {
+        if column1[i] > column2[i] {
+            distance += column1[i] - column2[i];
+        } else if column1[i] < column2[i] {
+            distance += column2[i] - column1 [i];
         }
-        println!("D: {d}\n");
     }
 
-    println!("Distance: {d}");
+    println!("Distance: {distance}");
 }
