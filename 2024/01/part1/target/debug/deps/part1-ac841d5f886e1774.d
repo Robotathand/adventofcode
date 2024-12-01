ae1ae6b541ba7d2e169204e540afd1f26ae8bf3f
robotathand <robotathand@gmail.com>
2024-12-01 22:35:22 +0000

    Remove target

--- 2024/01/part1/target/debug/deps/part1-ac841d5f886e1774.d
+++
@@ -1,5 +0,0 @@
-/home/luqman/Documents/Programming/adventofcode/2024/01/part1/target/debug/deps/libpart1-ac841d5f886e1774.rmeta: src/main.rs
-
-/home/luqman/Documents/Programming/adventofcode/2024/01/part1/target/debug/deps/part1-ac841d5f886e1774.d: src/main.rs
-
-src/main.rs:
