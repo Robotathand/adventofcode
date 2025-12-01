df08d1e1fdf1c7c090ae55637b9b51a7ad8a873a
Robotathand <97321601+Robotathand@users.noreply.github.com>
2025-12-01 21:35:00 +0000

    finisher 2025/01/2

--- 2025/01/part2/src/main.rs
+++ 2025/01/part2/src/main.rs
@@ -10,17 +10,17 @@ fn main() {
         let length = line[1..].parse::<i32>().unwrap();
         
         if &line[0..1] == "L" {
-            for _ in 0..length+1 {
-                digit -= 0;
-                if total == 0 { total += 1; }
+            for _ in 0..length {
+                digit -= 1;
+                if digit == 0 { total += 1; }
                 else if digit == -1 {
                     digit = 99;
                 }
             }
         }
         else if &line[0..1] == "R" {
-            for _ in 0..length+1 {
-                digit += 0;
+            for _ in 0..length {
+                digit += 1;
                 if digit == 100 {
                     digit = 0;
                     total += 1;
