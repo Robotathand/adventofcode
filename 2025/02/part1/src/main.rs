4c485564e0832cb191dd7dc97d43ca2284e266c3
Robotathand <97321601+Robotathand@users.noreply.github.com>
2025-12-02 21:57:37 +0000

    start 2025/02/1

---
+++ 2025/02/part1/src/main.rs
@@ -0,0 +1,28 @@
+use std::fs;
+
+fn main() {
+    let data = fs::read_to_string("../data.txt").unwrap();
+    let nums: Vec<&str> = data.split(['-', ',']).collect();
+    let mut total = 0;
+    for i in 0..nums.len() / 2 - 1 {
+        // println!("{:?}", nums);
+        // println!("{} {}", nums[2 * i], nums[2 * i + 1]);
+        /* if nums[2 * i].len() % 2 != 0 && nums[2 * i].len() == nums[2 * i + 1].len() {
+            break;
+        } */
+        let begin = nums[2 * i].parse::<i64>().unwrap();
+        let end = nums[2 * i + 1].parse::<i64>().unwrap();
+        for num in begin..end + 1 {
+            let num_string = num.to_string();
+            let length = num_string.len();
+            if length % 2 == 0
+            /* && &num_string[..1] != "0" */
+            {
+                if num_string[..length / 2] == num_string[length / 2..] {
+                    total += num;
+                }
+            }
+        }
+    }
+    println!("{total}");
+}
